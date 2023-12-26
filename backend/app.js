d077f7d2b774fe8703ea3c8b41ba69f6823d3247
OkcuYazilimci <umutuygun.u@gmail.com>
2023-12-26 14:16:50 +0300

    14:16 | 16 Dec

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/user-routes.js';
 import blogRouter from './routes/blog-routes.js';
-import authRouter from './routes/Auth-Routes.js';
 import swaggerUi from 'swagger-ui-express';
+import bodyParser from 'body-parser';
 import YAML from 'yamljs';
 import morgan from 'morgan';
-import exphbs from 'express-handlebars';
 import connectDB from './config/db.js';
 import passport from 'passport';
 import cors from 'cors'; // Add this line
 import session from 'express-session';
-import './config/passport.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
@@ -21,15 +19,14 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(bodyParser.json());
+
 //For swagger
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //load config
 dotenv.config({ path: './config/config.env' });
-
-// passport config
-import './config/passport.js'
 
 const PORT = process.env.PORT || 6000;
 
@@ -50,11 +47,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); // it shows http methods on console
 }
 
-// HandleBars
-const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs', layoutsDir: './views/layouts' });
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
-
 // Sessions
 const MongoStore = (await import('connect-mongo')).default; 
 
@@ -71,10 +63,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Static folder
-const publicPath = join(__dirname, 'public');
-app.use(express.static(publicPath));
-
 //sessions
 app.use(session({
     secret: 'keyboard car',
@@ -82,13 +70,9 @@ app.use(session({
     saveUninitialized: false,
 }));
 
-//passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
 //app routes
 app.use(express.json());
 app.use("/api-user", router);
 app.use("/api-blog", blogRouter);
-app.use("/auth", authRouter);
+
 
